b45fdfe71a989aef81f8e6f715035ea41780ec83
souleymane <souleymanedialloo@yahoo.fr>
2020-10-12 16:27:27 +0200

    some change

--- page/views.py
+++ page/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from covoiturage.models import Covoiturage
 
 
 # Create your views here.
 def home(request):
-    return render(request, "page/index.html")
+    covoiturages = Covoiturage.objects.all()[:4]
+    context = {'covoiturages': covoiturages}
+    return render(request, "page/index.html", context)
